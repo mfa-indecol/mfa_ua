b03f59234eea14b7c025133d42ddfddc748bc8b8
nilsdi <ni.dittrich@web.de>
2024-10-10 13:38:24 +0200

    made pip installable

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mfa_ua
-Various functions related to uncertainty analysis
+Various tools related to uncertainty analysis in material flow analysis (MFA).
+Some of the modules were specifically built for the NTNU course TEP4285, others come from research projects.
+
+## current subpackages
+- monte_carlo: designed for TEP4285, contains module for setting up parameters, sampling, and the monte carlo analysis
+- parameter_estimation: fitting a lognormal distribution on lower and upper bounds and a mode, inversing lognormal into a right skewed distribution
+- sensitivity_analysis: computing sensitivities for any fucntion using sympy; plotting sensitivity heatmaps
--- setup.py
+++ setup.py
@@ -1,4 +1,29 @@
-import setuptools
-setuptools.setup()
+from setuptools import setup, find_packages
+
+setup(
+    name="mfa_ua",
+    version="0.1.0",
+    author="Nils Dittrich",
+    author_email="nils.dittrich@ntnu.no",
+    description="Uncertainty analysis tools for MFA",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/mfa_ua",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        # not all versions and combination of versions are tested
+        "numpy>=1.18.0,<2.0.0",
+        "scipy>=1.4.0,<2.0.0",
+        "matplotlib>=3.1.0,<4.0.0",
+        "tqdm>=4.41.0,<5.0.0",
+        "sympy>=1.5.0,<2.0.0",
+    ],
+)
 
 # install with "python -m pip install -e ."
